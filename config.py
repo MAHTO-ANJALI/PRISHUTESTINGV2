85ff04633a1c299a437f9aeff0bd5702ee08269f
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-05-13 21:01:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MAHTO-ANJALI/PRISHUMUSIC",
+    "https://t.me/NEON_SUKOON",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
